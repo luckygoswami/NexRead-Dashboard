cc18aa629904fda37ee9482b3743f2e39187ee08
Deepak Goswami <125822925+luckygoswami@users.noreply.github.com>
2025-11-16 21:11:31 +0530

    feat: add footer & navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,10 @@
+export default function Navbar() {
+  return (
+    <div className="p-5 flex justify-between border border-blue-100">
+      {/* Logo */}
+      <div>NexRead</div>
+      {/* Search */}
+      <div>search</div>
+    </div>
+  );
+}
