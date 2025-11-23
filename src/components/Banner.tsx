8af1188dfdc61ec7ef67360ceadfdc5df49d2d55
Deepak Goswami <125822925+luckygoswami@users.noreply.github.com>
2025-11-23 22:07:09 +0530

    style: add mobile media queries on Banner

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 export function Banner() {
   return (
-    <div className="bg-[#fafafb] rounded-md w-full py-10 px-35 text-center flex flex-col gap-5 items-center">
+    <div className="bg-[#fafafb] rounded-md w-full md:py-10 md:px-35 py-5 px-3 text-center flex flex-col gap-5 items-center">
       <h1 className="text-4xl font-bold">Discover Your Next Great Read</h1>
       <p className="text-sm">
         Your gateway to a world of knowledge. Explore a vast collection of
@@ -10,12 +10,14 @@ export function Banner() {
         discover new authors, and expand your horizons with ease.
       </p>
       <Image
-        src={'/assets/book-reading.jpg'}
+        src={
+          'https://images.unsplash.com/photo-1506880018603-83d5b814b5a6?q=80&w=774&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+        }
         alt="Banner"
         sizes="100vw"
         height={0}
         width={0}
-        className="w-[400px] h-[200px] rounded-lg object-cover"
+        className="md:w-[400px] md:h-[200px] w-[90%] h-40 rounded-lg object-cover"
         priority
       />
     </div>
